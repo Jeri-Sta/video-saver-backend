46eb0ff750c0543edb638a0a64396b7f137e30aa
Jeri-Sta <jerielstarosky@hotmail.com>
2024-08-23 14:06:35 -0300

    commit inicial

---
+++ src/main/java/br/com/videosaver/VideoSaverApplication.java
@@ -0,0 +1,13 @@
+package br.com.videosaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoSaverApplication.class, args);
+	}
+
+}
